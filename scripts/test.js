b259eeebe0d952e7a7c07ce96e99cb754faace10
Raymond Zhou <somecrazyidiot@gmail.com>
2012-09-25 19:29:23 -0400

    Deleted commented-out code.

--- scripts/test.js
+++ scripts/test.js
@@ -53,22 +53,8 @@ Terrain.prototype.toString = function() {
   }
 };
 
-// var canvas;
-// var contex;
-// var width  = 800;
-// var height = 600;
+
 var trackMouse = false;
-// TODO: Not really WebGL right now.
-// function initWebGL() {
-//   canvas = document.getElementById( "test" );
-//   canvas.width = width;
-//   canvas.height = height;
-
-//   contex = canvas.getContext( "2d" );
-
-//   canvas.addEventListener( 'mousedown', onMouseDown );
-//   canvas.addEventListener( 'mousemove', onMouseMove );
-// }
 
 function onMouseMove( event ) {
   event.pageX -= _game._canvas.offsetLeft;
@@ -97,12 +83,6 @@ function draw() {
   gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
 }
 
-// var easingFunctions = new Easing();
-
-// var characters = [];
-// var bullets = [];
-// var effects = [];
-
 window.requestAnimFrame = (function() {
   return window.requestAnimationFrame       ||
          window.webkitRequestAnimationFrame ||
@@ -113,28 +93,6 @@ window.requestAnimFrame = (function() {
             window.setTimeout( callback, 1000 / 60 );
          };
 })();
-
-// function drawCanvas() {
-//   update();
-//   contex.clearRect( 0, 0, width, height );
-
-//   // contex.fillStyle = 'rgba( 200, 200, 200, 1.0 )';
-//   // contex.fillRect( 0, 0, width, height );
-
-//   // Draw characters.
-//   var i;
-//   for ( i = characters.length - 1; i >= 0; i-- ) {
-//     characters[i].draw( contex );
-//   }
-
-//   // Draw bullets.
-//   for ( i = bullets.length - 1; i >= 0; i-- ) {
-//     bullets[i].draw( contex );
-//   }
-
-//   requestAnimFrame( drawCanvas );
-// }
-
 
 function inArray( elem, array ) {
   for ( var i = array.length - 1; i >= 0; i-- )
@@ -154,50 +112,6 @@ function direction( x0, y0, x1, y1 ) {
   return Math.atan2( y1 - y0, x1 - x0 );
 }
 
-// var prevTime = Date.now();
-// var currTime;
-
-// function update() {
-//   currTime = Date.now();
-//   var elapsedTime = currTime - prevTime;
-//   prevTime = currTime;
-
-//   // Update characters.
-//   var i;
-//   for ( i = characters.length - 1; i >= 0; i-- ) {
-//     characters[i].update( elapsedTime );
-//   }
-
-//   // Update bullets.
-//   for ( i = bullets.length - 1; i >= 0; i-- ) {
-//     bullets[i].update( elapsedTime );
-//   }
-
-//   for ( i = effects.length - 1; i >= 0; i-- ) {
-//     effects[i].update( elapsedTime );
-//   }
-// }
-
-// function initGame() {
-//   // "Good guy"
-//   characters.push( new Character( 400, 400, 0, 0, 200, 1.0, 10 ) );
-//   characters[0].velocity = {
-//     x: 0,
-//     y: 0
-//   };
-//   characters[0].firing = false;
-
-//   // "Bad guy"
-//   characters.push( new Character( 200, 200, 200, 0, 0, 1.0, 10 ) );
-//   characters[1].setTeam(1);
-// }
-
-// function init() {
-//   initGame();
-//   initWebGL();
-//   console.log( "test" );
-//   drawCanvas();
-// }
 
 function loop() {
   _game.tick();
