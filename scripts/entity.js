1052b47418fe0ae685a557fa765573ff8cf49383
Raymond Zhou <somecrazyidiot@gmail.com>
2012-09-24 23:33:39 -0400

    Added entity.js.
     Moved Character, Entity, and Circle classes to entity.js.

---
+++ scripts/entity.js
@@ -0,0 +1,173 @@
+// Entity ----------------------------------------------------------------------
+var Entity = function( x, y, red, green, blue, alpha ) {
+  this.x = x;
+  this.y = y;
+
+  this.red   = red;
+  this.green = green;
+  this.blue  = blue;
+  this.alpha = alpha;
+
+  this.lifeTime = 0;
+
+  this.velocity = {
+    x: 0.25,
+    y: 0.25
+  };
+};
+
+Entity.prototype.update = function( elapsedTime ) {
+  this.x += elapsedTime * this.velocity.x;
+  this.y += elapsedTime * this.velocity.y;
+};
+
+
+// Circle ----------------------------------------------------------------------
+var Circle = function( x, y, red, green, blue, alpha, radius ) {
+  Entity.call( this, x, y, red, green, blue, alpha );
+  this.radius = radius;
+
+  this.lifeTime = 0;
+  this.maximumAge = 2000;
+};
+
+Circle.prototype = new Entity();
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.update = function( elapsedTime ) {
+  Entity.prototype.update.call( this, elapsedTime );
+
+  if ( this.radius > this.x ) {
+    this.x = this.radius;
+    this.velocity.x = -this.velocity.x;
+  }
+  if ( this.x + this.radius > width ) {
+    this.x = width - this.radius;
+    this.velocity.x = -this.velocity.x;
+  }
+  if ( this.radius > this.y ) {
+    this.y = this.radius;
+    this.velocity.y = -this.velocity.y;
+  }
+  if ( this.y + this.radius > height ) {
+    this.y = height - this.radius;
+    this.velocity.y = -this.velocity.y;
+  }
+};
+
+Circle.prototype.draw = function( ctx ) {
+  ctx.beginPath();
+  ctx.arc(
+    Math.round( this.x ),
+    Math.round( this.y ),
+    Math.round( this.radius ),
+    0,
+    Math.PI * 2,
+    true
+  );
+  ctx.fillStyle = 'rgba( ' + Math.round( this.red )   +
+                  ', '     + Math.round( this.green ) +
+                  ','      + Math.round( this.blue )  +
+                  ','      + this.alpha + ' )';
+  ctx.fill();
+};
+
+
+// Character -------------------------------------------------------------------
+var Character = function( x, y, red, green, blue, alpha, radius ) {
+  Circle.call( this, x, y, red, green, blue, alpha, radius );
+
+  this.firing      = true;
+  this.fireTime    = 0;
+  this.fireRate    = 200;
+  this.bulletSpeed = 0.5;
+
+  this.health = 100;
+
+  this.isHit     = false;
+  this.hitTime   = 0;
+  this.hitLength = 100;
+};
+
+Character.prototype = new Circle();
+Character.prototype.constructor = Character;
+
+Character.prototype.fireAt = function( x, y ) {
+  var bullet = new Bullet( this.x, this.y, 0, 0, 0, 1.0, 2, this );
+
+  bullet.velocity.x = x - this.x;
+  bullet.velocity.y = y - this.y;
+
+  var magnitude = Math.sqrt( bullet.velocity.x *
+                             bullet.velocity.x +
+                             bullet.velocity.y *
+                             bullet.velocity.y );
+  bullet.velocity.x /= magnitude / this.bulletSpeed;
+  bullet.velocity.y /= magnitude / this.bulletSpeed;
+
+  bullets.push( bullet );
+};
+
+Character.prototype.update = function( elapsedTime ) {
+  Circle.prototype.update.call( this, elapsedTime );
+  // Find enemy.
+  var enemy;
+  for ( var i = characters.length - 1; i >= 0; i-- ) {
+    if ( characters[i] !== this ) {
+      enemy = characters[i];
+    }
+  }
+
+  if ( this.firing && this.fireTime <= 0 ) {
+    this.fireTime = this.fireRate;
+    if ( enemy !== null && enemy !== undefined ) {
+      this.fireAt( enemy.x, enemy.y );
+    } else {
+      this.fireAt(
+        this.x + this.velocity.x * 50,
+        this.y + this.velocity.y * 50
+      );
+    }
+  } else {
+    this.fireTime -= elapsedTime;
+  }
+};
+
+Character.prototype.hit = function() {
+  if ( !this.isHit ) {
+    this.isHit = true;
+    // Effects chain. Nasty syntax, but it works.
+    effects.push(
+      new Effect(
+        this,
+        {
+          radius: 15,
+          red: 200
+        },
+        60,
+        Easing.easeInOutCubic,
+        undefined,
+        (function() {
+          return {
+            effect: new Effect(
+              this,
+              {
+                radius: -15,
+                red: -200
+              },
+              150,
+              Easing.linear,
+              undefined,
+              (function() {
+                this.isHit = false;
+                return {
+                  effect: undefined
+                };
+              })
+            )
+          };
+        })
+      )
+    );
+  }
+};
--- scripts/weapons.js
+++ scripts/weapons.js
@@ -1,3 +1,4 @@
+// Weapon ----------------------------------------------------------------------
 var Weapon = function( damage, rate, range ) {
   this.firing = true;
   this.time   = 0;
@@ -7,12 +8,93 @@ var Weapon = function( damage, rate, range ) {
 
 Weapon.prototype.update = function( elapsedTime ) {
   this.time += elapsedTime;
-  if ( this.time >= this.rate )
-    this.time = 0;
+  if ( this.time >= this.rate ) {
+    this.firing = true;
+    this.time   = 0;
+  }
 };
 
 
-
-var Gun = function() {
-
+// Bullet ----------------------------------------------------------------------
+var Bullet = function( x, y, red, green, blue, alpha, radius, shooter ) {
+  Circle.call( this, x, y, red, green, blue, alpha, radius );
+  this.shooter = shooter;
+  this.collides = true;
 };
+
+Bullet.prototype = new Circle();
+Bullet.prototype.constructor = Bullet;
+
+Bullet.prototype.update = function( elapsedTime ) {
+  Circle.prototype.update.call( this, elapsedTime );
+
+  var removeBullet = false;
+  if ( this.collides ) {
+    var distance;
+    var i;
+    for ( i = characters.length - 1; i >= 0; i-- ) {
+      if ( characters[i] !== this.shooter ) {
+        distance = Math.sqrt( ( this.x - characters[i].x ) *
+                              ( this.x - characters[i].x ) +
+                              ( this.y - characters[i].y ) *
+                              ( this.y - characters[i].y ) );
+        if ( distance < this.radius + characters[i].radius ) {
+          characters[i].hit();
+          removeBullet = true;
+        }
+      }
+    }
+  }
+
+  if ( this.lifeTime > this.maximumAge ) {
+    removeBullet = true;
+  } else {
+    this.lifeTime += elapsedTime;
+  }
+
+  if ( removeBullet ) {
+    this.velocity.x = 0;
+    this.velocity.y = 0;
+    this.collides = false;
+    effects.push(
+      new Effect(
+        this,
+        {
+          radius: 5,
+          alpha: -1.0
+        },
+        500,
+        Easing.easeOutQuad,
+        undefined,
+        (function() {
+          removeFromArray( this, bullets );
+          return {
+            effect: undefined
+          };
+        })
+      )
+    );
+  }
+};
+
+
+// Gun -------------------------------------------------------------------------
+var Gun = function( damage, rate, range ) {
+  Weapon.call( this, damage, rate, range );
+  this.target = null;
+};
+
+Gun.prototype = new Weapon();
+Gun.prototype.constructor = Gun;
+
+Gun.prototype.update = function( elapsedTime ) {
+  Weapon.prototype.update.call( this, elapsedTime );
+  if ( this.firing && this.target !== null ) {
+
+  }
+};
+
+Gun.prototype.setTarget = function( entity ) {
+  this.target = entity;
+};
+
