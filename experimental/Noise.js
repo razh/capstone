9c24f78054e972f347f4887952b086e32ef8dac4
Raymond Zhou <somecrazyidiot@gmail.com>
2012-10-11 08:26:03 -0400

    SimplexNoise converted to have static methods.

--- experimental/Noise.js
+++ experimental/Noise.js
@@ -1,10 +1,16 @@
 var Noise = function() {
   var test = 6;
+  return {
+    this.noise = function( string ) {
+  console.log("5" + string);
+
+    }
+  };
 };
 
-Noise.noise = function( string ) {
-  console.log("5" + string);
-};
+// Noise.noise = function( string ) {
+//   console.log("5" + string);
+// };
 
 
 Noise.noise("sdsd");
--- experimental/SimplexNoise.js
+++ experimental/SimplexNoise.js
@@ -24,7 +24,7 @@ var Grad3 = function( x, y, z ) {
 };
 
 // Simplex noise in 2D.
-var SimplexNoise = function() {
+var SimplexNoise = (function() {
   var grad3 = [
     new Grad3( 1,  1,  0),
     new Grad3(-1,  1,  0),
@@ -83,93 +83,95 @@ var SimplexNoise = function() {
     return g.x * x + g.y * y;
   }
 
-  this.noise = function( xin, yin ) {
-    // Noise contributions from the three corners.
-    var n0, n1, n2;
+  return {
+    noise: function( xin, yin ) {
+      // Noise contributions from the three corners.
+      var n0, n1, n2;
 
-    // Skew the input space to determine which simplex cell we're in.
+      // Skew the input space to determine which simplex cell we're in.
 
-    // Hairy factor for 2D
-    var s = ( xin + yin ) * F2;
-    var i = fastFloor( xin + s );
-    var j = fastFloor( yin + s );
-    var t = ( i + j ) * G2;
+      // Hairy factor for 2D
+      var s = ( xin + yin ) * F2;
+      var i = fastFloor( xin + s );
+      var j = fastFloor( yin + s );
+      var t = ( i + j ) * G2;
 
-    // Unskew the cell origin back to (x,y) space.
-    var X0 = i - t;
-    var Y0 = j - t;
+      // Unskew the cell origin back to (x,y) space.
+      var X0 = i - t;
+      var Y0 = j - t;
 
-    // The x,y distances from the cell origin.
-    var x0 = xin - X0;
-    var y0 = yin - Y0;
+      // The x,y distances from the cell origin.
+      var x0 = xin - X0;
+      var y0 = yin - Y0;
 
-    // For the 2D case, the simplex shape is an equilateral triangle.
-    // Determine which simplex we are in.
+      // For the 2D case, the simplex shape is an equilateral triangle.
+      // Determine which simplex we are in.
 
-    // Offsets for second (middle) corner of simplex in (i, j) coords.
-    var i1, j1;
-    // Lower triangle, XY order: (0, 0) -> (1, 0) -> (1, 1).
-    if ( x0 > y0 ) {
-      i1 = 1;
-      j1 = 0;
+      // Offsets for second (middle) corner of simplex in (i, j) coords.
+      var i1, j1;
+      // Lower triangle, XY order: (0, 0) -> (1, 0) -> (1, 1).
+      if ( x0 > y0 ) {
+        i1 = 1;
+        j1 = 0;
+      }
+      // Upper triangle, YX order: (0, 0) -> (0, 1) -> (1, 1).
+      else {
+        i1 = 0;
+        j1 = 1;
+      }
+
+      // A step of (1, 0) in (i, j) means a step of (1 - c, -c) in (x, y), and
+      // a step of (0, 1) in (i, j) means a step of (-c, 1 - c) in (x, y), where
+      // c = (3 - sqrt(3)) / 6.
+
+      // Offsets for middle corner in (x,y) unskewed coords.
+      var x1 = x0 - i1 + G2;
+      var y1 = y0 - j1 + G2;
+
+      // Offsets for last corner in (x,y) unskewed coords.
+      var x2 = x0 - 1.0 + 2.0 * G2;
+      var y2 = y0 - 1.0 + 2.0 * G2;
+
+      // Work out the hashed gradient indices of the three simplex corners.
+      var ii = i & 255;
+      var jj = j & 255;
+
+      var gi0 = permMod12[ ii + perm[ jj ] ];
+      var gi1 = permMod12[ ii + i1 + perm[ jj + j1 ] ];
+      var gi2 = permMod12[ ii + 1 + perm[ jj + 1 ] ];
+
+      // Calculate the contribution from the three corners.
+      var t0 = 0.5 - x0 * x0 - y0 * y0;
+      if ( t0 < 0 )
+        n0 = 0.0;
+      else {
+        t0 *= t0;
+        // (x,y) of grad3 used for 2D gradient.
+        n0 = t0 * t0 * dot( grad3[ gi0 ], x0, y0 );
+      }
+
+      var t1 = 0.5 - x1 * x1 - y1 * y1;
+      if ( t1 < 0 )
+        n1 = 0.0;
+      else {
+        t1 *= t1;
+        n1 = t1 * t1 * dot( grad3[ gi1 ], x1, y1 );
+      }
+
+      var t2 = 0.5 - x2 * x2 - y2 * y2;
+      if ( t2 < 0 )
+        n2 = 0.0;
+      else {
+        t2 *= t2;
+        n2 = t2 * t2 * dot( grad3[ gi2 ], x2, y2 );
+      }
+
+      // Add contributions from each corner to get the final noise value.
+      // The result is scaled to return values in the interval [-1,1].
+      return 70.0 * ( n0 + n1 + n2 );
     }
-    // Upper triangle, YX order: (0, 0) -> (0, 1) -> (1, 1).
-    else {
-      i1 = 0;
-      j1 = 1;
-    }
+  }
+}) ();
 
-    // A step of (1, 0) in (i, j) means a step of (1 - c, -c) in (x, y), and
-    // a step of (0, 1) in (i, j) means a step of (-c, 1 - c) in (x, y), where
-    // c = (3 - sqrt(3)) / 6.
-
-    // Offsets for middle corner in (x,y) unskewed coords.
-    var x1 = x0 - i1 + G2;
-    var y1 = y0 - j1 + G2;
-
-    // Offsets for last corner in (x,y) unskewed coords.
-    var x2 = x0 - 1.0 + 2.0 * G2;
-    var y2 = y0 - 1.0 + 2.0 * G2;
-
-    // Work out the hashed gradient indices of the three simplex corners.
-    var ii = i & 255;
-    var jj = j & 255;
-
-    var gi0 = permMod12[ ii + perm[ jj ] ];
-    var gi1 = permMod12[ ii + i1 + perm[ jj + j1 ] ];
-    var gi2 = permMod12[ ii + 1 + perm[ jj + 1 ] ];
-
-    // Calculate the contribution from the three corners.
-    var t0 = 0.5 - x0 * x0 - y0 * y0;
-    if ( t0 < 0 )
-      n0 = 0.0;
-    else {
-      t0 *= t0;
-      // (x,y) of grad3 used for 2D gradient.
-      n0 = t0 * t0 * dot( grad3[ gi0 ], x0, y0 );
-    }
-
-    var t1 = 0.5 - x1 * x1 - y1 * y1;
-    if ( t1 < 0 )
-      n1 = 0.0;
-    else {
-      t1 *= t1;
-      n1 = t1 * t1 * dot( grad3[ gi1 ], x1, y1 );
-    }
-
-    var t2 = 0.5 - x2 * x2 - y2 * y2;
-    if ( t2 < 0 )
-      n2 = 0.0;
-    else {
-      t2 *= t2;
-      n2 = t2 * t2 * dot( grad3[ gi2 ], x2, y2 );
-    }
-
-    // Add contributions from each corner to get the final noise value.
-    // The result is scaled to return values in the interval [-1,1].
-    return 70.0 * ( n0 + n1 + n2 );
-  };
-};
-
-var sn = new SimplexNoise();
-console.log(sn.noise(5,5));
+// var sn = new SimplexNoise();
+console.log(SimplexNoise.noise(5,5));
