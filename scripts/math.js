11dc5cc3aa73fd11f491d6c4641057b631b48834
Raymond Zhou <somecrazyidiot@gmail.com>
2012-10-15 20:54:20 -0400

    More work on vector math.

--- scripts/math.js
+++ scripts/math.js
@@ -4,40 +4,51 @@ var MatrixArray = ( typeof Float32Array !== 'undefined' ) ?
                   Float32Array : Array;
 
 // MatrixArray (backwards compatibility) is taken from toji's gl-matrix.js.
-Vec2.create = function( a, b ) {
+vec2.create = function( a, b ) {
   return new MatrixArray( [ a, b ] );
 };
 
-Vec2.add = function( a, b, dest ) {
+vec2.add = function( a, b, dest ) {
+  if ( !dest ) dest = b;
   dest[0] = a[0] + b[0];
   dest[1] = a[1] + b[0];
+  return dest;
 };
 
-Vec2.subtract = function( a, b, dest ) {
-
+vec2.subtract = function( a, b, dest ) {
+  if ( !dest ) dest = b;
+  dest[0] = a[0] - b[0];
+  dest[1] = a[1] - b[1];
+  return dest;
 };
 
-Vec2.multiply = function( a, b, dest ) {
+vec2.multiply = function( a, b, dest ) {
+  if ( !dest ) dest = b;
   dest[0] = a[0] * b[0];
   dest[1] = a[1] * b[1];
+  return dest;
 };
 
-Vec2.divide = function( a, b, dest ) {
+vec2.divide = function( a, b, dest ) {
+  if ( !dest ) dest = b;
   dest[0] = a[0] / b[0];
   dest[1] = a[1] / b[1];
+  return dest;
 };
 
-Vec2.scale = function( vec, scalar, dest ) {
+vec2.scale = function( vec, scalar, dest ) {
+  if ( !dest ) dest = vec;
   dest[0] = vec[0] * scalar;
   dest[1] = vec[1] * scalar;
+  return dest;
 };
 
-Vec2.dist = function( a, b ) {
+vec2.dist = function( a, b ) {
   var x = b[0] - a[0],
       y = b[1] - a[1];
   return Math.sqrt( x * x + y * y );
 };
 
-Vec2.dot = function() {
+vec2.dot = function() {
 
 };
